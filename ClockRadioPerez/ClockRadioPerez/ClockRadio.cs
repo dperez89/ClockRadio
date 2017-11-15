897ad19d9bc3f7262a134ff19f35a01ad4ddffe2
David Perez <d.perez89@aol.com>
2017-11-15 14:16:13 -0600

    adding in functionality to display current time from computer clock / adding in member variables, constructor method, member methods.

---
+++ ClockRadioPerez/ClockRadioPerez/ClockRadio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockRadioPerez
+{
+    public class ClockRadio
+    {
+        public string color;
+        public bool isOn;
+        public string currentTime;
+        public string currentAlarmTime;
+
+        public ClockRadio()
+        {
+            color = "brown";
+            isOn = true;
+            currentTime = DateTime.Now.Hour.ToString() + ":" + DateTime.Now.Minute.ToString();
+            currentAlarmTime = "12:00";
+        }
+
+        public string ViewTime()
+        {
+            Console.WriteLine(currentTime);
+            return currentTime;
+        }
+        //member methods
+    }
+}
---
+++ ClockRadioPerez/ClockRadioPerez/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockRadioPerez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClockRadio davidClockRadio = new ClockRadio();
+            string currentTime = davidClockRadio.ViewTime();
+            Console.ReadKey();
+        }
+    }
+}
